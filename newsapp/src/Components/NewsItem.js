ced8cbfa3368d446bb6835a56061662062624681
sohammanjrekar <mrsohammanjrekar@gmail.com>
2022-07-29 23:52:12 +0530

    update

--- newsapp/src/Components/NewsItem.js
+++ newsapp/src/Components/NewsItem.js
@@ -6,8 +6,8 @@ export class NewsItem extends Component {
       <div className="card" style={{ width: "25rem"}}>
         <img src={url} className="card-img-top" alt={title} style={{ height: "15rem"}}></img>
         <div className="card-body">
-          <h5 className="card-title">{title.slice(0,45)}...</h5>
-          <p className="card-text">{description.slice(0,88)}...</p>
+          <h5 className="card-title">{title}...</h5>
+          <p className="card-text">{description}...</p>
           <a href={newsurl} className="btn btn-primary">
             Go somewhere
           </a>
